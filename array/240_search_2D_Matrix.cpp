31e93bf3216f7dacf429f9e090676ffb2d1b6071
priyanshupundir003-ctrl <priyanshupundir003@gmail.com>
2026-08-10 13:13:53 +0530

    Create 240_search_2D_Matrix.cpp

---
+++ array/240_search_2D_Matrix.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& arr, int target) {
+        int m = arr.size(),  n = arr[0].size();
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(arr[i][j]==target) return true;
+            }
+        }
+        return false;
+    }
+};
